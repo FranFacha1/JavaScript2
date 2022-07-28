df0fbfec42f96119fd6a18d9d8f46e34e18e4bd6
francisco bellotti <franciscobellotti2002@gmail.com>
2022-07-27 21:15:24 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+// Condicionales
+// let nombre = "guille";
+
+// if (1 == 1) {
+//     console.log("1 es igual a 1")
+// } else {
+//     console.log("No es igual a 1")
+// }
+
+
+// let nombre1 = prompt("Cual es tu nombre");
+
+// if (nombre1 == "agustin") {
+//     console.log("tu apellido es capuano")
+// } else if (nombre1 == "francisco") {
+//     console.log("tu apellido es bellotti")
+// } else if (nombre1 == "francisco") {
+//     console.log("tu apellido es bellotti")
+// } else {
+//     console.log("ingrese un nombre") 
+//  } 
+
+
+// Ciclos / iteraciones
+
+// for (desde, hasta, actualizacion) {
+
+// }
+
+
+// Ejemplo tabla del 3
+
+const tabla = 3;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(tabla * i);
+}
+
+// Ejemplos de sumatoria de todos los numeros
+
+let sumatoria = 0;
+
+for(let i = 0; i <= 5; i++) {
+    sumatoria = sumatoria * i;
+    console.log(sumatoria);
+}
+
+
+// for(let i = 0; i >= 0; i++) {
+//     console.log("Capu puto");
+// }
